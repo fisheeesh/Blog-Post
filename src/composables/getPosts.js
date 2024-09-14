8a1bf4310d136f0d9b3d2dc6300ac405137f5d9c
fisheeesh <swanphyo444@gmail.com>
2024-09-14 22:25:55 +0700

    composable function using object destructing

---
+++ src/composables/getPosts.js
@@ -0,0 +1,23 @@
+import { ref } from "vue";
+
+let getPosts = () => {
+  let posts = ref([]);
+  let error = ref("");
+  let load = async () => {
+    try {
+      let response = await fetch("http://localhost:3000/posts");
+      if (response.status === 404) {
+        throw new Error("Not Found URL!");
+      }
+      let datas = await response.json();
+      posts.value = datas;
+    } catch (e) {
+      console.log(e.message);
+      error.value = e.message;
+    }
+  };
+
+  return { posts, error, load }
+};
+
+export default getPosts
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,31 +15,13 @@
 
 <script>
 import PostsList from '../components/PostsList'
-import { onMounted, onUnmounted, onUpdated, ref } from 'vue';
-
+import getPosts from '@/composables/getPosts';
 
 export default {
   components: { PostsList },
   setup() {
-    let posts = ref([])
-
-    let error = ref("")
-
-
-    let load = async () => {
-      try {
-        let response = await fetch('http://localhost:3000/posts')
-        if(response.status === 404){
-          throw new Error('Not Found URL!')
-        }
-        let datas = await response.json()
-        posts.value = datas
-      }
-      catch(e){
-        console.log(e.message)
-        error.value = e.message
-      }
-    }
+    
+    let { posts, error, load } = getPosts() // {posts, error, load} will return and its gonna work like object destructing
 
     load()
 
